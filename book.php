f796375e580e943619d4118213658ec4a1a94d4a
Alexandra SALVADOR <apintosalvador19@gmail.com>
2024-04-01 22:27:38 +0200

    challenge 1 variables

---
+++ book.php
@@ -0,0 +1,10 @@
+<?php
+
+$bookName="Voyage avec un âne dans les Cévennes";
+$isRead=true;
+$bookYear=2017;
+$price=6.20;
+echo $bookName."<br/>";
+echo $isRead."<br/>";
+echo $bookYear."<br/>";
+echo $price." €<br/>";
